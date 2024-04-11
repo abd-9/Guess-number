087c2377ec7912d382c5f8b00076be1a9f635011
Abdalkareem Ataya <64447489+abd-9@users.noreply.github.com>
2024-04-12 01:51:22 +0400

    added redux logic and maped data

--- app/(Guess Number)/game/page.tsx
+++ app/(Guess Number)/game/page.tsx
@@ -1,144 +1,49 @@
-import Card from "@/components/home/card";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
-import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
+import { useAppSelector } from "@/app/redux/hooks";
+import ActiveRoundSection from "@/components/game/(round board)";
+import LoginSection from "@/components/game/login";
+import ScoreBar from "@/components/game/score";
+import RankSection from "@/components/game/rank";
+import ChatSection from "@/components/game/chat";
+import { GameBoard } from "@/components/game/gameBoard";
+import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
 
 export default async function Home() {
-  const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/steven-tey/precedent",
-    {
-      ...(process.env.GITHUB_OAUTH_TOKEN && {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      }),
-      // data will revalidate every 24 hours
-      next: { revalidate: 86400 },
-    },
-  )
-    .then((res) => res.json())
-    .catch((e) => console.log(e));
-
   return (
     <>
-      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <a
-          href="https://twitter.com/steventey/status/1613928948915920896"
-          target="_blank"
-          rel="noreferrer"
-          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-          <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Precedent
-          </p>
-        </a>
-        <h1
-          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
-          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
-        >
-          Building blocks for your Next project
-        </h1>
-        <p
-          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
-          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-        >
-          An opinionated collection of components, hooks, and utilities for your
-          Next.js project.
-        </p>
-        <div
-          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
-          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
-        >
-          s
+      <div className="grid h-5/6 w-full grid-flow-col grid-cols-10 grid-rows-8 gap-4 px-4 py-4">
+        <div className=" col-span-3 row-span-8   ">
+          <GameBoard></GameBoard>
+        </div>
+        <div className="col-span-7 row-span-1  w-full">
+          <ScoreBar />
+        </div>
+        <div className="col-span-7   row-span-7 w-full rounded-xl  border-2 border-primaryBorder bg-background p-4 text-white">
+          Main content
         </div>
       </div>
-      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-        <Card
-          key={"Ddd"}
-          title={"title "}
-          description={"sss"}
-          demo={<ComponentGrid />}
-          large={true}
-        />
 
-        {/* {features.map(({ title, description, demo, large }) => (
-          <Card
-            key={title}
-            title={title}
-            description={description}
-            demo={
-              title === "Beautiful, reusable components" ? (
-                <ComponentGrid />
-              ) : (
-                demo
-              )
-            }
-            large={large}
-          />
-        ))} */}
+      <div className="grid h-1/4 w-full grid-cols-2 gap-4 px-4 ">
+        <div className="  col-span-1  w-full overflow-hidden rounded-xl border-primaryBorder ">
+          <h4 className="mb-2 font-bold">
+            <MilitaryTechIcon fontSize="small" />
+            Ranking
+          </h4>
+          <RankSection />
+        </div>
+
+        <div className="col-span-1  w-full overflow-hidden ">
+          <h4 className="mb-2 font-bold">
+            <QuestionAnswerIcon fontSize="small" /> Chat
+          </h4>
+          <div
+            className=" rounded-xl  border-2 border-primaryBorder bg-background"
+            style={{ height: "calc(100% - 2em)" }}
+          >
+            <ChatSection />
+          </div>
+        </div>
       </div>
     </>
   );
 }
-
-const features = [
-  {
-    title: "Beautiful, reusable components",
-    description:
-      "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/), and [Framer Motion](https://framer.com/motion)",
-    large: true,
-  },
-  {
-    title: "Performance first",
-    description:
-      "Built on [Next.js](https://nextjs.org/) primitives like `@next/font` and `next/image` for stellar performance.",
-    demo: <WebVitals />,
-  },
-  {
-    title: "One-click Deploy",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <a href={DEPLOY_URL}>
-        <Image
-          src="https://vercel.com/button"
-          alt="Deploy with Vercel"
-          width={120}
-          height={30}
-          unoptimized
-        />
-      </a>
-    ),
-  },
-  {
-    title: "Built-in Auth + Database",
-    description:
-      "Precedent comes with authentication and database via [Auth.js](https://authjs.dev/) + [Prisma](https://prisma.io/)",
-    demo: (
-      <div className="flex items-center justify-center space-x-20">
-        <Image alt="Auth.js logo" src="/authjs.webp" width={50} height={50} />
-        <Image alt="Prisma logo" src="/prisma.svg" width={50} height={50} />
-      </div>
-    ),
-  },
-  {
-    title: "Hooks, utilities, and more",
-    description:
-      "Precedent offers a collection of hooks, utilities, and `@vercel/og`",
-    demo: (
-      <div className="grid grid-flow-col grid-rows-3 gap-10 p-10">
-        <span className="font-mono font-semibold">useIntersectionObserver</span>
-        <span className="font-mono font-semibold">useLocalStorage</span>
-        <span className="font-mono font-semibold">useScroll</span>
-        <span className="font-mono font-semibold">nFormatter</span>
-        <span className="font-mono font-semibold">capitalize</span>
-        <span className="font-mono font-semibold">truncate</span>
-      </div>
-    ),
-  },
-];
